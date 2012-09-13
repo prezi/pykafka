77eda32c80a4e4b337683cee5c03ae2d54a46d1f
Zsolt Dollenstein <zsol.zsol@gmail.com>
2012-09-13 15:44:00 -0700

    include kazoo in install_dependencies

--- setup.py
+++ setup.py
@@ -3,15 +3,16 @@
 import setuptools
 
 setuptools.setup(
-  name = 'pykafka',
-  version = '2.4.3',
-  license = 'MIT',
-  description = open('README.md').read(),
-  author = "Datadog, Inc.",
-  author_email = "packages@datadoghq.com",
-  url = 'https://github.com/datadog/pykafka',
-  platforms = 'any',
-  packages = ['kafka'],
-  zip_safe = True,
-  verbose = False,
+    name='pykafka',
+    version='2.4.3',
+    license='MIT',
+    description=open('README.md').read(),
+    author="Datadog, Inc.",
+    author_email="packages@datadoghq.com",
+    url='https://github.com/datadog/pykafka',
+    platforms='any',
+    packages=['kafka'],
+    install_requires={'kazoo': ['>= 0.5', '< 0.6']},
+    zip_safe=True,
+    verbose=False,
 )
